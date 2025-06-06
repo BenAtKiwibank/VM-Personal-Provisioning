d7761fff3be3660641eecb000d83c7b49dec7680
ben wang <ben.wang@kiwibank.co.nz>
2025-06-06 13:21:56 +1200

    fix path bug

--- tools/install-oh-my-zsh.sh
+++ tools/install-oh-my-zsh.sh
@@ -5,7 +5,7 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
     echo "oh-my-zsh installed successfully!"
     
-    ZSH_CONFIG_FILE="$PROVISIONING_DIR/tools/zsh_config.content"
+    ZSH_CONFIG_FILE="$HOME/vm-personal-provisioning/tools/zsh_config.content"
     if [ -f "$ZSH_CONFIG_FILE" ]; then
         echo "Applying custom zsh configuration..."
         cat "$ZSH_CONFIG_FILE" >> ~/.zshrc
