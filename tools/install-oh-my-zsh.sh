c1759e361a0388f0e8ab4639d2c4f23931c528ae
ben wang <ben.wang@kiwibank.co.nz>
2025-07-22 08:46:51 +1200

    amodify

--- tools/install-oh-my-zsh.sh
+++ tools/install-oh-my-zsh.sh
@@ -2,14 +2,18 @@
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
     echo "Installing oh-my-zsh..."
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
     echo "oh-my-zsh installed successfully!"
     
     ZSH_CONFIG_FILE="$HOME/vm-personal-provisioning/tools/zsh_config.content"
     if [ -f "$ZSH_CONFIG_FILE" ]; then
         echo "Applying custom zsh configuration..."
         cat "$ZSH_CONFIG_FILE" >> ~/.zshrc
+        source ~/.zshrc
         echo "Custom zsh configuration applied!"
+    else
+        echo "Error: ZSH configuration file not found at $ZSH_CONFIG_FILE"
+        exit 1
     fi
 else
     echo "oh-my-zsh is already installed, skipping installation."
